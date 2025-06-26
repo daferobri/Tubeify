dc342a5828a7267edaa750d551aafb6685671828
David Rodriguez <144063814+daferobri@users.noreply.github.com>
2025-06-26 00:20:20 -0400

    Added feature to see songs in a user's playlist

--- tubeify.py
+++ tubeify.py
@@ -1,14 +1,15 @@
 import os
+from pprint import pp
 from dotenv import load_dotenv
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
 load_dotenv()
 
-CLIENT_ID = os.environ.get('SPOTIPY_CLIENT_ID')
-CLIENT_SECRET = os.environ.get('SPOTIPY_CLIENT_SECRET')
-REDIRECT_URI = 'http://127.0.0.1:1234'
-SCOPE = 'user-library-read'
+CLIENT_ID = os.environ.get("SPOTIPY_CLIENT_ID")
+CLIENT_SECRET = os.environ.get("SPOTIPY_CLIENT_SECRET")
+REDIRECT_URI = "http://127.0.0.1:1234"
+SCOPE = "user-library-read"
 
 spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(
     client_id=CLIENT_ID,
@@ -17,3 +18,28 @@ spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(
     scope=SCOPE
 ))
 
+results = spotify.current_user_playlists()["items"]
+playlist_names = [playlist["name"] for playlist in results]
+
+print("Playlists:" + "\n" + "--------------------")
+for index, name in enumerate(playlist_names):
+    print(f"{index + 1}. {name}")
+print()
+
+while True:
+    try:
+        playlist_index = int(input("Choose a number: "))
+    except ValueError:
+        continue
+    print()
+    if playlist_index > 0 and playlist_index <= len(playlist_names):
+        playlist_index -= 1
+        break
+
+songs = spotify.playlist_items(results[playlist_index]["id"], limit=20)["items"]
+tracks = [song["track"] for song in songs]
+track_names_and_artists = [(track["name"], track["artists"][0]["name"]) for track in tracks]
+
+print("Songs:"+ "\n" + "--------------------")
+for name, artist in track_names_and_artists:
+    print(f"{name} - {artist}")
